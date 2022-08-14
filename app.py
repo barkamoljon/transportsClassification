528137b3d99f950fbf7ff60a3a0995eb81e3746e
barkamoljon <97020905+barkamoljon@users.noreply.github.com>
2022-08-15 02:13:37 +0500

    Add files via upload
    transportClassification

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pathlib
+import plotly.express as px
+temb = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+from fastai.vision.all import *
+
+# title
+st.title("Transportni klassifikatsiya qiluvchi model")
+
+# rasmni joylash
+file = st.file_uploader('Rasm yuklash', type=(['png','jpg','jpeg','gif','svg']))
+
+if file is not None:
+    img = Image.open(file)
+    st.image(img, caption = 'Yuklangan rasm')
+    if file:
+      #st.image(file)
+      #PIL convert
+      img = PILImage.create(file)
+      #model
+      model = load_learner('transport_model.pkl')
+
+      # prediction
+      pred, pred_id, probs = model.predict(img)
+      st.success(f'Prognoz:{pred}')
+      st.info(f'Ehtimollik:{probs[pred_id]*100: .1f}%')
+
+      #plotting
+      fig = px.bar(x=probs*100, y=model.dls.vocab)
+      st.plotly_chart(fig)
